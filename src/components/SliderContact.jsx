5686b44113748b8a8fe9197245adc7bbb17318d6
Joshua Candia <joshuacandia74@gmail.com>
2023-08-31 20:58:32 -0300

    update(Contact/Form):sizes changed

--- src/components/SliderContact.jsx
+++ src/components/SliderContact.jsx
@@ -1,12 +1,14 @@
 //components
 import LandingBackground from './LandingBackground/LandingBackground';
-
+import ContactForm from './ContactForm';
 function SliderContact() {
    return (
-      <section className=' text-center h-screen w-screen relative overflow-hidden bg-orange-500 flex justify-start items-center'>
-         <div className='flex justify-center w-full '>
+      <section className='bg-[#060918] text-center h-screen w-screen relative overflow-hidden flex justify-start items-center'>
+         <div className='w-full flex flex-col items-center justify-center'>
             <h2>Contact</h2>
+            <ContactForm />
          </div>
+
          <LandingBackground />
       </section>
    );
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -35,100 +35,100 @@ function Tech() {
    }, []);
    return (
       <div className=' w-full h-full grid grid-cols-8 gap-4 justify-start items-center lg:gap-8 lg:grid-cols-4'>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <DiHtml5
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <DiCss3Full
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <DiSass
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiTailwindcss
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
          {/* Second row */}
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <DiJsBadge
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <DiReact
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiNextdotjs
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiTypescript
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
          {/* third row */}
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiRedux
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiAxios
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiReactrouter
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiJest
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
          {/* fourth row */}
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <DiNodejs
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiExpress
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <BiLogoPostgresql
                color='white'
                size={isScreenSmall ? 20 : 40}
             />
          </div>
-         <div class='col-span-1 hover:animate-wiggle-more'>
+         <div className='col-span-1 hover:animate-wiggle-more'>
             <SiSequelize
                color='white'
                size={isScreenSmall ? 20 : 40}
