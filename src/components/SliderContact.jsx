eb73e610e33382fc888b918e876468683e5ca1c1
Joshua Candia <joshuacandia74@gmail.com>
2023-09-25 11:01:49 -0300

    fix(Client/Contact):fix notebook sizes

--- src/components/SliderContact.jsx
+++ src/components/SliderContact.jsx
@@ -9,10 +9,10 @@ import { FiInstagram } from 'react-icons/fi';
 import { BiLogoLinkedinSquare } from 'react-icons/bi';
 function SliderContact() {
    return (
-      <section className='bg-[#060918] text-center h-screen w-screen relative overflow-hidden flex justify-center items-center'>
+      <section className='bg-[#060918] text-center h-screen w-screen relative overflow-hidden flex justify-center items-between'>
          <MatrixRainComponent />
          <div className=' absolute w-[80%] h-1/6 lg:h-15 xl:h-1/3 top-36 flex justify-center items-center rounded-xl text-white'>
-            <div className='pb-10 flex text-gray-400 flex-col items-start xl:items-center'>
+            <div className='pb- flex text-gray-400 flex-col items-start xl:items-center'>
                <h2 className='text-white text-xl md:text-lg lg:text-2xl xl:text-4xl pb-2 flex w-[80%] justify-start lg:justify-center'>
                   Contact me
                </h2>
@@ -64,12 +64,12 @@ function SliderContact() {
                </div>
             </div>
          </div>
-         <div className=' rounded-xl absolute bottom-24  lg:bottom-36  w-[80%] lg:w-[35%] h-1/'>
+         <div className=' rounded-xl absolute bottom-24  lg:bottom-8 xl-36  w-[80%] lg:w-[35%] h-1/'>
             <h5 className='text-large lg:text-xl text-[#4112A4]'>Source Code</h5>
             <h2 className='text-2xl lg:text-4xl p-2 text-white '>
                Behind the simulation
             </h2>
-            <p className='text-gray-500 text-sm lg:text-lg'>
+            <p className='text-gray-500 text-sm lg:text-md xl:text-lg'>
                You take the <span className='text-white'>blue pill</span> - you keep
                scrolling, you leave this site and believe whatever you want to
                believe. You take the <span className='text-white'>red pill</span> -
